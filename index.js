f4b06705fe2c5d66940882c71c6a42c62406832c
Phished <d.macias.apps@gmail.com>
2023-09-07 22:52:03 -0700

    Add player board container, add multiple card renderings

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Application = PIXI.Application
+const Container = PIXI.Container
+const Sprite = PIXI.Sprite
+const Texture = PIXI.Texture
 
 let app = new Application({
     width: innerWidth,
@@ -7,12 +10,41 @@ let app = new Application({
 
 document.body.appendChild(app.view)
 
-let sprite = PIXI.Sprite.from("./assets/Character_Card.webp")
-sprite.scale.set(.15);
+const CHARACTER_CARD_BACK = Texture.from("./assets/Character_Card.webp")
 
-app.stage.addChild(sprite)
+const playerBoard = new Container();
+
+app.stage.addChild(playerBoard);
 
 
-app.ticker.add((delta) => {
+for (let i = 0; i < 3; i++) {
 
-})
+    const cardBack = new Sprite(CHARACTER_CARD_BACK)
+    cardBack.scale.set(.15)
+    cardBack.x = i * 10
+    playerBoard.addChild(cardBack)
+}
+
+
+
+// let cardScale = .15
+
+// let card = PIXI.Sprite.from("./assets/Character_Card.webp")
+
+// card.scale.set(.15);
+
+// card.on("mouseover", (event) => {
+
+// })
+
+
+// card.eventMode = "dynamic"
+
+// app.stage.addChild(card)
+
+
+
+// app.ticker.add((delta) => {
+//     cardScale = .25
+//     card.rerender()
+// })
